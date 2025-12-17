6734e733526afbf330c3d5c10c16f05b2df91314
hieuvo <vonamhieu.work@gmail.com>
2025-12-17 14:05:18 +0700

    Initial Commit

---
+++ Film camera/Film_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Film_cameraApp.swift
+//  Film camera
+//
+//  Created by mac on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Film_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
